c86e502c8ffc9ace7582862d9847e9eeeb0a8108
tententgc <nyman3527@gmail.com>
2022-02-09 18:59:52 +0700

    update new features and setup lib

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Education',
-    'Operating System :: Microsoft :: Windows :: Windows 10',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3'
-]
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
 
 setup(
+    long_description_content_type='text/plain',
     name='tententgclibrary',
     version='0.0.3',
     description='Mathematics calculate library',    
