40db96d9778da888f0ec595d21e768d5bbfd1a6e
tententgc <nyman3527@gmail.com>
2022-02-09 18:11:35 +0700

    update library add functions

--- tententgclib/__init__.py
+++ tententgclib/__init__.py
@@ -17,7 +17,27 @@ def mean(a):
 def listfind_odd(a):
     return [x for x in a if x % 2 == 1]
 
-def listfing_even(a):
+def listfind_even(a):
     return [x for x in a if x % 2 == 0]
 
+def stack(a,b):
+    for i in range(1,a+1): 
+        print(b*i)
+        
+def reverse_stack(a,b):
+    for i in range(a,0,-1):
+        print(b*i)
+        
+def factorial(a):
+    if a == 0:
+        return 1
+    else:
+        return a * factorial(a-1)
 
+def lower_alphabet():
+    a = 'abcdefghijklmnopqrstuvwxyz'
+    return a
+
+def upper_alphabet():
+    a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return a
